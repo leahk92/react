1635b98f9489ff81d898df64f178e27f3892f5ce
Leah <leahkang92@gmail.com>
2024-10-17 16:55:03 +0900

    state recap

--- 연습.js
+++ 연습.js
@@ -2,18 +2,47 @@ const root = document.getElementById("root");
 
 let counter = 0;
 
-function countUp() {
-  counter += 1;
-  ReactDOM.render(<Container />, root);
-}
+function App() {
+  const [amount, setAmount] = React.useState(0);
+  const [flipped, setFlipped] = React.useState(false);
+  const onChange = (event) => {
+    setAmount(event.target.value);
+  };
+  const rerset = () => setAmount(0);
+  const onFlip = () => {
+    rerset();
+    setFlipped((current) => !current);
+  };
 
-function Container() {
   return (
     <div>
-      <h3>Total clicks: {counter}</h3>
-      <button onClick={countUp}>Click me</button>
+      <h1 className="hi">Super Convertor</h1>
+      <div>
+        <label htmlFor="minutes">Minutes</label>
+        <input
+          value={flipped ? amount * 60 : amount}
+          id="minutes"
+          placeholder="Minutes"
+          type="number"
+          onChange={onChange}
+          disabled={flipped} //초기화값 false - 활성화된 상태
+        />
+      </div>
+      <div>
+        <label htmlFor="hours">Hours</label>
+        <input
+          value={flipped ? amount : amount / 60}
+          id="hours"
+          placeholder="Hours"
+          type="number"
+          disabled={!flipped} //true - 비활성화 상태
+          onChange={onChange}
+        />
+      </div>
+      <button onClick={rerset}>Reset</button>
+      <button onClick={onFlip}>Flipped</button>
     </div>
   );
 }
 
-ReactDOM.render(<Container />, root);
+ReactDOM.render(<App />, root);
